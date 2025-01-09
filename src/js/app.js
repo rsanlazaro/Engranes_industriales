1750b86561c0e5419df9aa57eff43917fad9fa6b
rsanlazaro <rsanlazaro@hotmail.com>
2025-01-09 14:53:11 -0600

    Version 1.0

---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+const preloaderDisplayTime = 100;
+
+window.addEventListener('load', function () {
+    const preloader = document.getElementById('preloader-1');
+    setTimeout(function () {
+        preloader.style.display = 'none';
+    }, preloaderDisplayTime);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    languages();
+})
+
+function languages() {
+    const btn = document.getElementById('languageBtn');
+    const spanish = document.getElementById('spanish');
+    const english = document.getElementById('english');
+    const french = document.getElementById('french');
+    btn.addEventListener('click', () => {
+        console.log("click");
+    });
+    spanish.addEventListener('click', () => {
+        console.log("Español");
+    });
+    english.addEventListener('click', () => {
+        console.log("English");
+    });
+    french.addEventListener('click', () => {
+        console.log("Francais");
+    });
+}
